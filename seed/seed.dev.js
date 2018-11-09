ceb614eac9882587719d41b83ce2c25fba958c16
KWright16 <42691009+KWright16@users.noreply.github.com>
2018-11-09 16:35:51 +0000

    altered configs

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -1,6 +1,6 @@
 const seedDB = require('./seed');
 const mongoose = require('mongoose');
-const DB_URL = require('../config');
+const DB_URL = process.env.NODE_ENV === 'production' ? process.env : require('../config')
 const rawData = process.env.NODE_ENV === 'test' ? require('./testData/index') : require('./devData/index')
 
 mongoose.connect(DB_URL)
